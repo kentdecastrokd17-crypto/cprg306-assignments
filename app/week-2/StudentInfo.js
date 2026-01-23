ec3515f6fdf8cac8791e03d7915e3e4be58339dd
kudo <kent.decastrokd17@gmail.com>
2026-01-22 22:46:01 -0700

    week 2 assignment
    new page for week-2, react component, edit root page

---
+++ app/week-2/StudentInfo.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return (
+        <div>
+            <p>Name: Kent De Castro</p>
+            <p>
+                GitHub: <Link href = "https://github.com/kentdecastrokd17-crypto/cprg306-assignments.git">kentdecastrokd17-crypto/cprg306-assignments</Link>
+            </p>
+        </div>
+    )
+}
