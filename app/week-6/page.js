74da7820794e45ed7f476bfe469f1e5ee9b163e7
kudo <kent.decastrokd17@gmail.com>
2026-03-06 21:15:18 -0700

    Update page.js

--- app/week-6/page.js
+++ app/week-6/page.js
@@ -1,18 +1,22 @@
-// page.js logic check
+"use client";
+
+import { useState } from "react";
+import ItemList from "./ItemList";
+import NewItem from "./NewItem";
+import itemsData from "./item.json";
+
 export default function Page() {
   const [items, setItems] = useState(itemsData);
 
   function handleAddItem(newItem) {
-    setItems([...items, newItem]); // Properly updating state with the new item
+    setItems([...items, newItem]);
   }
 
   return (
     <main className="p-10 bg-gray-600 min-h-screen">
-      <h1 className="text-3xl font-bold mb-6 text-white">Shopping List</h1>
-      <NewItem onAddItem={handleAddItem} /> 
-      <div className="mt-8">
-        <ItemList items={items} />
-      </div>
+      <h1 className="text-3xl font-bold mb-6">Shopping List</h1>
+      <NewItem onAddItem={handleAddItem} />
+      <ItemList items={items} />
     </main>
   );
 }
